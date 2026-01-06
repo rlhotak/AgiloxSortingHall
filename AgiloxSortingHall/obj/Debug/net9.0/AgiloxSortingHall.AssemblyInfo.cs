87155c68fa7e52b3d81e133f7f4ecab65a51b228
johnnyprochazka <jan.prochazka@heinz-glas.com>
2026-01-06 14:29:15 +0100

    back button on skladnik.cshtml

--- AgiloxSortingHall/obj/Debug/net9.0/AgiloxSortingHall.AssemblyInfo.cs
+++ AgiloxSortingHall/obj/Debug/net9.0/AgiloxSortingHall.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgiloxSortingHall")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+399dd3cad41cb16f58b4066af6cbf48e82fbb16b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+901023ba9be39e992e61cd5353931aeab8b7687b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgiloxSortingHall")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgiloxSortingHall")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
