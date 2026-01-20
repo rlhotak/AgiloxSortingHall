99d52bbe79e4fc01f81b574f2a767ca9a3931e37
Jan Procházka <Jan.Prochazka@heinz-glas.com>
2026-01-20 11:09:22 +0100

    detailurl kontrola allowed for pending status to cancel

--- AgiloxSortingHall/obj/Debug/net9.0/AgiloxSortingHall.AssemblyInfo.cs
+++ AgiloxSortingHall/obj/Debug/net9.0/AgiloxSortingHall.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgiloxSortingHall")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61198c1ddb7c8cf388eeeba7deb3088888d570bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f428d3369771a457658ba6be1700ce03335e10")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgiloxSortingHall")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgiloxSortingHall")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
