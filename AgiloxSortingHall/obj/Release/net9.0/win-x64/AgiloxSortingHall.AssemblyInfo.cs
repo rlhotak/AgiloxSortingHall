6fade0baa4516353dec207a386ed66009c3bf585
Jan Procházka <Jan.Prochazka@heinz-glas.com>
2026-01-21 14:00:47 +0100

    fit skladnik on different row counts

--- AgiloxSortingHall/obj/Release/net9.0/win-x64/AgiloxSortingHall.AssemblyInfo.cs
+++ AgiloxSortingHall/obj/Release/net9.0/win-x64/AgiloxSortingHall.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgiloxSortingHall")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f428d3369771a457658ba6be1700ce03335e10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45f2747c9ea004901f9865368a0642c9e635cc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgiloxSortingHall")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgiloxSortingHall")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
