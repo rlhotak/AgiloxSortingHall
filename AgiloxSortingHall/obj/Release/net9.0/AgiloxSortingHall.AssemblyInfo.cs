8f005a090bda4cf62ab4a8d1302086a1b0dd9c72
johnnyprochazka <jan.prochazka@heinz-glas.com>
2026-06-03 11:36:50 +0200

    visual button pickup empty pallet

--- AgiloxSortingHall/obj/Release/net9.0/AgiloxSortingHall.AssemblyInfo.cs
+++ AgiloxSortingHall/obj/Release/net9.0/AgiloxSortingHall.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgiloxSortingHall")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364a9e1c41205df49ab82710ff57c3e272780bcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51faca857909079e80f8c89a588e333bff03f799")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgiloxSortingHall")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgiloxSortingHall")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
